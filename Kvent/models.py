6a139d6abb6e8ca43971ee79eaf0cc089c7d6ddc
bleachjade <bleachjade@hotmail.com>
2020-10-15 19:13:31 +0700

    add user field

--- Kvent/models.py
+++ Kvent/models.py
@@ -1,3 +1,23 @@
+from typing import Optional
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Info(models.Model):
+    """User's model"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
+    phone_num = models.CharField('Phone Number', max_length=10, default="NOT SET")
+    email = models.EmailField('E-mail', max_length=254)
+    address = models.CharField("Address", max_length=125)
+
+
+    def get_name(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+
+    def get_number(self):
+        return self.phone_num
+
+    def get_email(self):
+        return self.email
+
+    def get_address(self):
+        return self.address

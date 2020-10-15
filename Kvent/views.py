e5c14e510e3ceb882e96a43dc1cb6f020ff285e7
bleachjade <bleachjade@hotmail.com>
2020-10-15 19:15:11 +0700

    add profile view

--- Kvent/views.py
+++ Kvent/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def profile(request):
+    """User's profile"""
+    user = request.user
+    info = request.user.info
+    context = {'user':user, 'info':info}
+    return render(request, 'BetterTogetherApp/profile.html', context)

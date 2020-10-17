3f7aa8837e72aa09bf77aaeb854f30b326002df2
forfeen <f.fin07@gmail.com>
2020-10-17 21:15:37 +0700

    Create model of event and config setting

--- Kvent/models.py
+++ Kvent/models.py
@@ -1,3 +1,28 @@
+# import datetime
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+myDate = datetime.now()
+formatedDate = myDate.strftime("%Y-%m-%d %H:%M:%S")
+class Event(models.Model):
+    """ Create the event """
+    event_name = models.TextField('Event Name', default="", max_length=50)
+    location = models.TextField('Location', default="", max_length=80)
+    description = models.TextField('Description', default="", max_length=100)
+    number_people = models.IntegerField("Number of people", default=2)
+    date_time = models.TextField('Date and Time', default=str(formatedDate))
+
+    def get_event_name(self):
+        return self.event_name
+
+    def get_location(self):
+        return self.location
+    
+    def get_description(self):
+        return self.description
+    
+    def get_time(self):
+        return self.date_time
+    
+    def get_number_people(self):
+        return self.number_people

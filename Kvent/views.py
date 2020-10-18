26fb7946be9970e0a33888f3fa84b5fbe4880f34
forfeen <f.fin07@gmail.com>
2020-10-18 20:18:40 +0700

    style index page and add views

--- Kvent/views.py
+++ Kvent/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
+from .models import Event
 
 def index(request):
-   return render(request, 'kvent/index.html')
+   event = Event.objects.all()
+   context = {'all_event': event}
+   return render(request, 'kvent/index.html', context)
 
 def create(request):
     return render(request, 'kvent/create-event-page.html')

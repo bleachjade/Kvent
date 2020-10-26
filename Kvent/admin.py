c39a25ae92c4e09d05c6b7b4ebc74485dd7034b8
bleachjade <bleachjade@hotmail.com>
2020-10-26 13:03:30 +0700

    display event detail in list_display

--- Kvent/admin.py
+++ Kvent/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Event
 
-admin.site.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        'event_name',
+        'location',
+        'short_description'
+    )
+
+
+admin.site.register(Event, EventAdmin)

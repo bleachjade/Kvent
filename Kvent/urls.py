7446538143b200a952a5464514af738a61cda36c
forfeen <f.fin07@gmail.com>
2020-10-27 14:34:05 +0700

    add delete event url

--- Kvent/urls.py
+++ Kvent/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('<int:event_id>/', views.detail, name='event-detail'),
     path('profile', views.profile, name='profile'),
     path('create',views.create_event, name='create-event'),
+    path('delete/<int:event_id>', views.delete_event, name='delete-event'),
 ]
 

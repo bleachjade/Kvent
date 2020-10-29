d1bbdea1480ebbfe14fa65997f33c73d66e816fc
bleachjade <bleachjade@hotmail.com>
2020-10-29 12:16:39 +0700

    move signup and login url from mysite to kvent and delete app name

--- Kvent/urls.py
+++ Kvent/urls.py
@@ -2,11 +2,13 @@ from django.urls import include, path
 from django.contrib.auth import views as auth_views
 from . import views
 
-app_name = "kvent"
+
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:event_id>/', views.detail, name='event-detail'),
     path('profile', views.profile, name='profile'),
     path('create',views.create_event, name='create-event'),
     path('delete/<int:event_id>', views.delete_event, name='delete-event'),
+    path('signup/', views.create_account , name='signup'),
+    path('login/', views.login, name='login')
 ]

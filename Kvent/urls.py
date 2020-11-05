4d07b482b3295aa1ec094ce7c6113e54b7ee1eba
Jirawadee Sampusri <jirawadeelovely@hotmail.com>
2020-11-05 12:24:18 +0700

    modified create account

--- Kvent/urls.py
+++ Kvent/urls.py
@@ -3,15 +3,17 @@ from django.contrib.auth import views as auth_views
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
+
 
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:event_id>/', views.detail, name='event-detail'),
-    path('profile', views.profile, name='profile'),
+    path('accounts/profile/', views.profile, name='profile'),
     path('create',views.create_event, name='create-event'),
     path('delete/<int:event_id>', views.delete_event, name='delete-event'),
-    path('signup/', views.create_account , name='signup'),
+    path('signup/', views.signup, name='signup'),
     path('', include('django.contrib.auth.urls')),
 ] 
 if settings.DEBUG:

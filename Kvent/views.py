6fa37690f8aaa855fd38ef79c3101b6475b61c50
Jirawadee Sampusri <jirawadeelovely@hotmail.com>
2020-11-05 12:19:51 +0700

    fix create account

--- Kvent/views.py
+++ Kvent/views.py
@@ -5,6 +5,10 @@ from django.contrib.auth import authenticate, login, logout
 from .models import Event, Info, User
 from .forms import EventForm
 from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import login, authenticate
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
   
 class IndexView(generic.ListView):
     """Show index view which is a list of all events."""
@@ -46,11 +50,23 @@ def create_event(request):
             return HttpResponseRedirect(reverse('index'))
     return render(request, 'Kvent/create-event-page.html', {'form': form})
 
-def create_account(request):
-    return render(request, 'registration/createaccount.html')
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/createaccount.html', {'form': form})
 
 def delete_event(request, event_id):
     event = Event.objects.get( pk=event_id)
     event.delete()
     return redirect('index')
+
 

4442305899e4299606f5504568513d4fd098abd8
BenZacs <BenZacs>
2020-11-07 07:24:14 +0700

    create form fr activity

--- Kvent/forms.py
+++ Kvent/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Event
+from .models import Event, Activity
 from django.forms.widgets import Textarea
 
 class EventForm(forms.ModelForm):
@@ -26,5 +26,20 @@ class EventForm(forms.ModelForm):
         number_people = forms.CharField(
             widget=forms.TextInput(
             attrs={'type':'input', 'id':'number-people'}
+            ))
+        photo = forms.ImageField()
+
+class ActivityForm(forms.ModelForm):
+    """ Form for inputing a new activity for event """
+    class Meta:
+        model = Activity
+        fields = ['activity_name', 'description', 'photo']
+        activity_name = forms.CharField(
+            widget=forms.TextInput(
+            attrs={'type':'input', 'id':'activity-name-input'}
+            ))
+        description = forms.CharField(
+            widget=forms.TextInput(
+            attrs={'type':'input', 'id':'description-input'}
             ))
         photo = forms.ImageField()

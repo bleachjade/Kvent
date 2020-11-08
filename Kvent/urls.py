2c4252eace844038f4cda9399962c4220e620640
BenZacs <BenZacs>
2020-11-08 16:39:54 +0700

    add urls that link to create activity page

--- Kvent/urls.py
+++ Kvent/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('<int:event_id>/', views.detail, name='event-detail'),
     path('profile', views.profile, name='profile'),
     path('create',views.create_event, name='create-event'),
+    path('<int:event_id>/create-activity',views.create_activity, name='create-activity'),
     path('delete/<int:event_id>', views.delete_event, name='delete-event'),
     path('signup/', views.create_account , name='signup'),
     path('', include('django.contrib.auth.urls')),

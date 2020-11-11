48d8887cac3abd11058c92068b2d4be88ff90c5e
bleachjade <bleachjade@hotmail.com>
2020-11-11 13:02:35 +0700

    add login and logout redirect to index page

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,3 +136,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AUTH_USER_MODEL = 'Kvent.User'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'

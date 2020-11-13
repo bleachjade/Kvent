5cae0d657242987ac6084479ad27150f376c01fe
bleachjade <bleachjade@hotmail.com>
2020-11-13 14:55:34 +0700

    change db setting to use postgres

--- mysite/settings.py
+++ mysite/settings.py
@@ -91,9 +91,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
+    }
+}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-
+DATABASE_URL=config('DATABASE_URL', default='secret')
 
 STATIC_URL = '/static/'
 

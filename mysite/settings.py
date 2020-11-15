d3e4cef86da8c20c1a7a4f778653128beffe261b
bleachjade <bleachjade@hotmail.com>
2020-11-15 19:03:22 +0700

    fix social auth and keep it in .env file

--- mysite/settings.py
+++ mysite/settings.py
@@ -151,6 +151,8 @@ LOGIN_URL = '/auth/login/google-oauth2/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY', default='secret')
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET', default='secret')
 
 try:
     from .local_settings import *

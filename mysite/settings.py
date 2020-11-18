9df330d1b40faf08ce120e8b4ab18214ff5026cb
forfeen <f.fin07@gmail.com>
2020-11-18 21:40:16 +0700

    add dropbox in setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,7 @@ from decouple import config
 
 import os
 import django_heroku
+import django_dropbox_storage
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

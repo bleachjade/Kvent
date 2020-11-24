3f3f18dc18e57bb851d8038366c8fa7fdd396d1d
bleachjade <bleachjade@hotmail.com>
2020-11-24 15:06:34 +0700

    try to fix heroku db error

--- mysite/settings.py
+++ mysite/settings.py
@@ -95,11 +95,9 @@ DATABASES = {
     }
 }
 
-
-
-# import dj_database_url
-# db_from_env = dj_database_url.config()
-# DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 STATIC_URL = '/static/'
 

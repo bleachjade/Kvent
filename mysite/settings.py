cef8c27bf3bd0c55f762f7a30c6b63310e1b9480
bleachjade <bleachjade@hotmail.com>
2020-12-06 09:46:44 +0700

    fix allowed host so that the program can run on local machine

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['kventeventapplication.herokuapp.com']
+ALLOWED_HOSTS = ['kventeventapplication.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition

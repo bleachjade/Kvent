13bb4f03d9089cff594bae187339472f39779d72
forfeen <f.fin07@gmail.com>
2020-12-13 17:12:09 +0700

    add detect that if phone number is string or float

--- Kvent/views.py
+++ Kvent/views.py
@@ -88,14 +88,19 @@ def signup(request):
         if form.is_valid():
             email = form.data.get('email')
             username = form.data.get('username')
+            phone_number = form.data.get('phone_num')
             if User.objects.filter(username=username).exists():
                 messages.error(request, "Your username is already taken!")
                 form = SignUpForm()
             else:
-                raw_password = form.data.get('raw_password')
-                user = authenticate(email=email,username=username, password=raw_password)
-                form.save()
-                return redirect(reverse('login'))
+                if isinstance(phone_number, str) or isinstance(phone_number, float):
+                    messages.error(request, "Your phone number must be integer!")
+                    form = SignUpForm()
+                else:    
+                    raw_password = form.data.get('raw_password')
+                    user = authenticate(email=email,username=username, password=raw_password)
+                    form.save()
+                    return redirect(reverse('login'))
     else:
         form = SignUpForm()
     return render(request,'registration/createaccount.html', {'form': form})
